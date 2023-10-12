72f45cd6da05c1d59aafdb1eba19be1d512ca5e7
Wize669 <william.zapata@epn.edu.ec>
2023-10-12 16:48:18 -0500

    Spy

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -9,13 +9,15 @@ const fakeBooks = [
   },
 ];
 
-const MongoLibStub = {
-  getAll: () => [...fakeBooks],
-  create: () => {},
-};
+const mockGetAll = jest.fn();
 
 // eslint-disable-next-line quotes
-jest.mock("../lib/mongo.lib", () => jest.fn().mockImplementation(() => MongoLibStub));
+jest.mock("../lib/mongo.lib", () =>
+  jest.fn().mockImplementation(() => ({
+    getAll: mockGetAll,
+    create: () => {},
+  }))
+);
 
 // eslint-disable-next-line quotes
 describe("Test for BooksService", () => {
@@ -30,22 +32,35 @@ describe("Test for BooksService", () => {
     // eslint-disable-next-line quotes
     test("should return list book", async () => {
       // Arrange
+      mockGetAll.mockResolvedValue(fakeBooks);
+
       // Act
       const books = await service.getBooks({});
-      console.log(books);
+      // console.log(books);
       // Assert
       expect(books.length).toEqual(1);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      // eslint-disable-next-line quotes
+      expect(mockGetAll).toHaveBeenCalledWith("books", {});
     });
 
     // eslint-disable-next-line quotes
     test("should return the name of the first book", async () => {
       // Arrange
+      mockGetAll.mockResolvedValue([
+        {
+          _id: 2,
+          // eslint-disable-next-line quotes
+          name: "Harry Potter2",
+        },
+      ]);
       // Act
       const books = await service.getBooks({});
-      console.log(books[0].name);
+      // console.log(books[0].name);
       // Assert
       // eslint-disable-next-line quotes
-      expect(books[0].name).toEqual("Harry Potter");
+      expect(books[0].name).toEqual("Harry Potter2");
     });
   });
 });
